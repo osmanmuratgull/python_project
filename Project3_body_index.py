5886813aef55babaf25ff09b2152fd48d9d3de65
Osman Muratgül <108622874+osmanmuratgull@users.noreply.github.com>
2022-10-17 16:05:05 +0300

    Update Project3_body_index.py

--- Project3_body_index.py
+++ Project3_body_index.py
@@ -1,19 +1,19 @@
-boy=int(input("Boyunuzu giriniz(cm cinsinden): "))
-kilo=float(input("kilonuzu giriniz: "))
-vki=kilo/((boy/100)**2)
-print("Vücut kitle index'iniz {}".format(round(vki,2)))
-print("Durumunuz: ")
+size=int(input("Enter your height (in cm): "))
+kg =float(input("enter your weight: "))
+vki=kg 16/((size/100)**2)
+print("Your body mass index {}".format(round(vki,2)))
+print("Your status: ")
 if vki <=18.5:
-    print("Zayıf")
-    print("{} kilogram almanız gerekiyor".format(round(18.5*((boy/100)**2)-kilo,2)))
+    print("Weak")
+    print("{} you need to gain kilograms".format(round(18.5*((size/100)**2)-kg ,2)))
 elif vki <=24.9:
     print("Normal")
 elif vki<=29.9:
-    print("Fazla kilolu")
-    print("{} kilogram vermeniz gerekiyor".format(round(kilo-24.9*((boy / 100) ** 2)),2))
+    print("Overweight")
+    print("{} you need to lose kilograms".format(round(kg -24.9*((size / 100) ** 2)),2))
 elif vki<=39.9:
-    print("Obez")
-    print("{} kilogram vermeniz gerekiyor".format(round(kilo - 24.9 * ((boy / 100) ** 2)), 2))
+    print("Obese")
+    print("{} you need to lose kilograms".format(round(kg  - 24.9 * ((size / 100) ** 2)), 2))
 else:
-    print("Aşırı obez")
-    print("{} kilogram vermeniz gerekiyor".format(round(kilo - 24.9 * ((boy / 100) ** 2)), 2))
+    print("extremely obese")
+    print("{} you need to lose kilograms".format(round(kg  - 24.9 * ((size / 100) ** 2)), 2))
