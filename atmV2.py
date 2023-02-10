bccb39400a59aa3a3a8f3198ac098935f051bb3c
Osman Muratgül <108622874+osmanmuratgull@users.noreply.github.com>
2023-02-10 13:56:36 +0300

    Add files via upload

---
+++ atmV2.py
@@ -0,0 +1,35 @@
+# ATM Automation V2!
+# @osmanmuratgull
+
+# User Login
+userName = str(input("Please Enter Your Username....:"))
+password = input("Please Enter Your Password.....:")
+print("1- Shows Balance!", " ", "2-Loads Money!",
+      " ", "3- Withdraws Money from Account!", "4-Quit")
+balance = 0
+
+while True:
+
+# User Authentication
+    if userName!= "Osman" and password!="123123":
+        print("Username or Password Incorrect....")
+        break
+# Value Parts
+    value=int(input("Please Enter a Value....: "))
+    if value==2:
+        process=int(input("Please Enter An Amount....:"))
+        balance+=process
+        print("Amount Remaining in Your Account...: ",balance)
+        
+    elif value==1:
+        print(balance)
+
+    elif value==3:
+        process=int(input("Please Enter An Amount....:"))
+        balance-=process
+        print("Amount Remaining in Your Account...:",balance)
+    elif value==4:
+        print("Leaving the ATM.....:")
+        break
+
+    
